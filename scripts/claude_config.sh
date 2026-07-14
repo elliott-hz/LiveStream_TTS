6330f612357495b271ae788832c7341e7a46cb94
elliott <elliott.cn.lee@gmail.com>
2026-07-14 10:52:38 +0800

    xxx

---
+++ scripts/claude_config.sh
@@ -0,0 +1,8 @@
+export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
+export ANTHROPIC_AUTH_TOKEN=sk-d166bd3d12404a5e9da39a4fcf57de3c
+export ANTHROPIC_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
+export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
+export CLAUDE_CODE_EFFORT_LEVEL=max
